7c63892444fbe380959ac24025a3ccc3635cb233
Agostinho Ferreira <agostinho008.af45@gmail.com>
2025-03-18 19:05:34 -0300

    reset

--- utils/generate_secret_key.py
+++
@@ -1,8 +0,0 @@
-import random
-chars = 'abcdefghijklmnopqrstuvwxyz' \
-        'ABCDEFGHIJKLMNOPQRSTUVXYZ' \
-        '0123456789' \
-        '#()^[]-_*%&=+/'
-
-SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
-print(SECRET_KEY)
